6182528541c2694f2acd6337147e28f1df7bc5bf
nirmal <nirmalit151@gmail.com>
2024-02-11 21:17:58 +0530

    Skill is added
    Works component is completed.
    Minor height issue resolved

--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { PageTitle } from '../pageTitle'
 
 const AboutMe = () => {
+  
   return (
-    <div className='h-screen'>AboutMe</div>
+    <div className='h-screen'>
+      <PageTitle titleName="about-me"/>
+    </div>
   )
 }
 
